327ee88a9811058b031605d7524d58b8a8690397
jeetmpatel1 <jeet.mpatel1@gmail.com>
2019-11-20 16:27:01 -0800

    added email functionality

---
+++ sendEmail.ps1
@@ -0,0 +1,17 @@
+﻿
+    $EmailTo = "email@gmail.com"  
+    $EmailFrom = "email@gmail.com"  
+    $Subject = "zx"  
+    $Body = "Test Body" 
+    $SMTPServer = "smtp.gmail.com" 
+    $filenameAndPath = "C:\Users\jmpatel\Desktop\Know.txt"  
+    $SMTPMessage = New-Object System.Net.Mail.MailMessage($EmailFrom,$EmailTo,$Subject,$Body)
+    $attachment = New-Object System.Net.Mail.Attachment($filenameAndPath)
+    $SMTPMessage.Attachments.Add($attachment)
+    $SMTPClient = New-Object Net.Mail.SmtpClient($SmtpServer, 587) 
+    $SMTPClient.EnableSsl = $true 
+    <# in the below line, password is the app password which can be retrieved from below stack overflow answer.
+    https://stackoverflow.com/questions/50078851/windows-powershell-smtp-server-requires-a-secure-connection/58964001#58964001
+    #>
+    $SMTPClient.Credentials = New-Object System.Net.NetworkCredential("email@gmail.com", "lggcvgwwofgcsizr");   
+    $SMTPClient.Send($SMTPMessage)
