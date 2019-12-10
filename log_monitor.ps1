6be7625e2dc8c2b4c97a4784067e67e9c13eff75
jeetmpatel1 <jeet.mpatel1@gmail.com>
2019-12-10 14:02:41 -0800

    corrected logic of finding log and zip files based on creation date instead of name
    Earlier command was finding out files with sepcific types of names only. In this commit , it select all the files which are created older than some particular date.

--- log_monitor.ps1
+++ log_monitor.ps1
@@ -1,33 +1,44 @@
-﻿$LogFilesPath = "C:\Users\jmpatel\Desktop\server-monitoring\logs\"
-$ZipFilesPath = "C:\Users\jmpatel\Desktop\server-monitoring\zips\"
-$LogDaysCriteria = $((Get-Date).AddDays(-1) | Get-Date -Format "yyyy-MM-dd")
-$ZipDaysCriteria = $((Get-Date).AddDays(-2) | Get-Date -Format "yyyy-MM-dd")
+$LogFilesPath = $PSScriptRoot+ "\logs\" 
+$ZipFilesPath = $PSScriptRoot+ "\zips\"
+
+$LogToZipArchivalDays=-5
+$ZipDeletionDays=-30
+
+$LogToZipArchivalDate=$((Get-Date).AddMinutes($LogToZipArchivalDays))
+$ZipDeletionDate=$((Get-Date).AddMinutes($ZipDeletionDays))
+
+$LogToZipArchivalDateToPrint = $( $LogToZipArchivalDate| Get-Date -Format "yyyy-MM-dd")
+$ZipDeletionDateToPrint = $($ZipDeletionDate | Get-Date -Format "yyyy-MM-dd")
+
 
 $CurrentTimeStamp= (Get-Date -UFormat "%Y-%m-%d %r %Z" | ForEach-Object { $_ -replace ":", "-" } | ForEach-Object { $_ -replace " ", "_" })    
 $LogFileOutputPath = $PSScriptRoot+ "\logs\zip_" +  $CurrentTimeStamp + ".log"
 
-$LogFilesSatisfytingCriteria = Get-ChildItem -File –Path "$LogFilesPath"  | Where-Object { $_.Name.StartsWith("$LogDaysCriteria") -and $_.Name.EndsWith("log") } 
-write-host "Started Log Monitoring Script"
-write-output "Started Log Monitoring Script" | Out-File -FilePath $LogFileOutputPath -Append
+$LogFilesSatisfytingCriteria = Get-ChildItem -File -Path "$LogFilesPath"  | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $LogToZipArchivalDate -and $_.Name.EndsWith("log") } 
+write-host "Started Log Monitoring Script" + $CurrentTimeStamp
+write-output "Started Log Monitoring Script $CurrentTimeStamp" | Out-File -FilePath $LogFileOutputPath -Append
 
 if($LogFilesSatisfytingCriteria -ne $Null ){
     write-output "Files which are added into the zip files will be ..." | Out-File -FilePath $LogFileOutputPath -Append
     write-output $LogFilesSatisfytingCriteria | Out-File -FilePath $LogFileOutputPath -Append
 
-    $ZipFileToSavePath = $( "$ZipFilesPath" + "$LogDaysCriteria" + ".zip")
+    $ZipFileToSavePath = $( "$ZipFilesPath" + "$LogToZipArchivalDateToPrint" + ".zip")
         $FileExists = Test-Path $ZipFileToSavePath
         if ($FileExists -eq $True) {
             Write-output "File already exists"| Out-File -FilePath $LogFileOutputPath -Append
-            $ZipFileToSavePath = $( "$ZipFilesPath" + "$LogDaysCriteria" + "$(Get-Date -UFormat '%r %Z' |  ForEach-Object { $_ -replace ':', '-' } | ForEach-Object { $_ -replace ' ', '_' })" + ".zip" )
+            $ZipFileToSavePath = $( "$ZipFilesPath" + "$LogToZipArchivalDateToPrint" + "$(Get-Date -UFormat '%r %Z' |  ForEach-Object { $_ -replace ':', '-' } | ForEach-Object { $_ -replace ' ', '_' })" + ".zip" )
         }
     write-output "Starting compressing process for $ZipFileToSavePath" | Out-File -FilePath $LogFileOutputPath -Append
     $LogFilesSatisfytingCriteria | Compress-Archive -DestinationPath "$ZipFileToSavePath"
     write-output "Ended compressing process for $ZipFileToSavePath" | Out-File -FilePath $LogFileOutputPath -Append
+
+    $LogFilesSatisfytingCriteria |  ForEach-Object { $_.Delete() }
+
 }else{
-    Write-Output "Unable to find any log files for date $LogDaysCriteria " | Out-File -FilePath $LogFileOutputPath -Append
+    Write-Output "Unable to find any log files older than date $LogToZipArchivalDateToPrint " | Out-File -FilePath $LogFileOutputPath -Append
 }
 
-$ZipFilesSatisfytingCriteria = Get-ChildItem -File –Path "$ZipFilesPath"  | Where-Object { $_.Name.StartsWith("$ZipDaysCriteria") -and $_.Name.EndsWith("zip") } 
+$ZipFilesSatisfytingCriteria = Get-ChildItem -File -Path "$ZipFilesPath"  | Where-Object { !$_.PSIsContainer -and $_.CreationTime -lt $ZipDeletionDate -and $_.Name.EndsWith("zip") } 
 
 if($ZipFilesSatisfytingCriteria -ne $Null ){
 
@@ -37,7 +48,7 @@ if($ZipFilesSatisfytingCriteria -ne $Null ){
     write-output "Ended deleting process for $ZipFileToDeletePath" | Out-File -FilePath $LogFileOutputPath -Append
 
 }else{
-    Write-Output "Unable to find any zip files for date $ZipDaysCriteria " | Out-File -FilePath $LogFileOutputPath -Append
+    Write-Output "Unable to find any zip files for date $ZipDeletionDateToPrint " | Out-File -FilePath $LogFileOutputPath -Append
 }
 
 write-output "Executed Successfully Log Monitoring Script" | Out-File -FilePath $LogFileOutputPath -Append
